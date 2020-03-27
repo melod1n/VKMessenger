4bdf0524efb3c346fbcb49d64a1cc42133f450a4
Danil Nikolaev <lischenkodev@gmail.com>
2020-03-27 13:46:37 +0300

    Фикс временной метки

--- app/src/main/java/ru/melod1n/vk/api/util/VKUtil.kt
+++ app/src/main/java/ru/melod1n/vk/api/util/VKUtil.kt
@@ -74,13 +74,11 @@ object VKUtil {
             val m1 = messages[i]
             val m2 = messages[i - 1]
 
-            //TODO: время показывается у последнего сообщения или если разница между сообщениями > 1 минуты
-
             val d1 = Util.removeTime(Date(m1.date * 1000L))
             val d2 = Util.removeTime(Date(m2.date * 1000L))
 
             if (d1 > d2) {
-                messages.add(i - 1, MessageAdapter.TimeStamp(SimpleDateFormat("dd MMM", Locale.getDefault()).format(d1)))
+                messages.add(i, MessageAdapter.TimeStamp(SimpleDateFormat("dd MMM", Locale.getDefault()).format(d1)))
             }
         }
     }
